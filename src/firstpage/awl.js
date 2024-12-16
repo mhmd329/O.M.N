d42f7ffd48d28dfb51cdd535490434e9afd968c8
mhmd329 <mhmdkhaled092@gmail.com>
2024-12-16 03:53:48 +0200

    Fix: Install missing FontAwesome react package

--- src/firstpage/awl.js
+++ src/firstpage/awl.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import  './firstpage.css'
-import Anv from './anv';
- const AwlPage = () => {
+import React from "react";
+import "./firstpage.css";
+import Anv from "./anv";
+const AwlPage = () => {
   return (
-    <div className='first'>
-<Anv/>
-  
-  <div className='text-box '>
-<h1>Goodbye junk food.
-<br />Hello super healthy meals.
-</h1>
+    <div className="first">
+      <Anv />
 
-<button><a className='  aww' href='#s'>i'm hungry</a></button>
-<button><a className='  tann' href='#d'>Show me more</a></button>
+      <div className="text-box">
+        <h1>
+          Goodbye junk food.
+          <br />
+          Hello super healthy meals.
+        </h1>
 
-</div>
-
-
+        <button>
+          <a className="aww" href="#s">
+            i'm hungry
+          </a>
+        </button>
+        <button>
+          <a className="tann" href="#d">
+            Show me more
+          </a>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default AwlPage;
